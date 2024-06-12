d33a95483eda8a791ca28fa351d4e52a3d1c23ff
mabou defo <sommetdefo3@gmail.com>
2024-06-12 11:20:54 +0100

    TUTORIEL_V1DAY6

--- jour_6/script.js
+++ jour_6/script.js
@@ -1,44 +1,23 @@
-const sous_contener = document.getElementsByClassName('sous_contener')
-const left = document.getElementsByClassName('left')
-const rigth = document.getElementsByClassName('rigth')
+const boxes = document.querySelectorAll('.box')
+console.log(boxes)
 
-console.log(sous_contener[0])
+window.addEventListener('scroll',checkBoxes)
 
-console.log(window.scrollY)
-let sous_contenerstyle0 = window.getComputedStyle(sous_contener[0])
-let valeurheight = sous_contenerstyle0.getPropertyValue('height')
-let height = parseInt(valeurheight)
-console.log(height)
+checkBoxes()
 
-window.addEventListener('scroll', function() {
-    if(this.window.scrollY > 100){
-        left[0].style.translate = '0px'
-    }
-    if(this.window.scrollY < 100){
-        left[0].style.translate = '-60vw'
-    }
-    if(this.window.scrollY > 320){
-        rigth[0].style.translate = '0px'
-    }
-    if(this.window.scrollY < 320){
-        rigth[0].style.translate = '60vw'
-    }
-    if(this.window.scrollY > 540){
-        left[1].style.translate = '0px'
-    }
-    if(this.window.scrollY < 540){
-        left[1].style.translate = '-60vw'
-    }
-    if(this.window.scrollY > 760){
-        rigth[1].style.translate = '0px'
-    }
-    if(this.window.scrollY < 760){
-        rigth[1].style.translate = '60vw'
-    }
-    if(this.window.scrollY > 980){
-        left[2].style.translate = '0px'
-    }
-    if(this.window.scrollY < 980){
-        left[2].style.translate = '-60vw'
-    }
-  });
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight/ 6 * 4
+    
+    boxes.forEach(box =>{
+        var boxTop = box.getBoundingClientRect().top
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
+}
+
+
+
