83074dcd9a50695a465673231112fbfd53e8fe6b
mabou defo <sommetdefo3@gmail.com>
2024-06-15 20:05:15 +0100

    RELEASE_V1DAY8

---
+++ jour_8/script.js
@@ -0,0 +1,9 @@
+const input1 = document.getElementById('name1')
+const input2 = document.getElementById('name2')
+const email = document.getElementById('email')
+
+
+const textemail = email.innerText.toString()
+
+console.log(input2)
+
