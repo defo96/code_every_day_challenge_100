fa88e4fa3a87626d0c978fe7fc4c543462f7db7d
mabou defo <sommetdefo3@gmail.com>
2024-06-16 19:36:29 +0100

    TUTORIEL_V1DAY10

--- jour_10/script.js
+++ jour_10/script.js
@@ -1,31 +1,40 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokebtn')
 
-const blague = document.getElementById('blague')
-const button = document.getElementById('button')
+jokeBtn.addEventListener('click',generateJoke)
 
-fetch("https://icanhazdadjoke.com/", {method: "GET", headers : 
-    {'Accept' :"text/plain"}, 
-})
-.then(response => response.text())
-.then(response => blague.textContent = response)
-.catch(error => alert("Erreur : " + error))
+generateJoke()
 
 
-button.addEventListener('click', () => {
-    fetch("https://icanhazdadjoke.com/", {method: "GET", headers : 
-        {'Accept' :"text/plain"}, 
-    })//fetch() renvoie une promesse. Lorsque nous aurons reçu
-    // une réponse du serveur, le gestionnaire then() de la
-    // promesse sera appelé avec la réponse
-    .then(response => response.text())//si la requête a réussi, le gestionnaire récupère
-    // la réponse sous forme de texte en appelant response.text(),
-    // Et renvoie immédiatement la promesse renvoyée par response.text().
-    .then(response => blague.textContent = response)//Quand response.text() a réussi, son gestionnaire `then()` est
-    // appelé avec le texte et nous copions celui-ci dans la boîte
-    // blague.
-    .catch(error => blague.textContent = `Erreur lors de la récupération du text vers https://icanhazdadjoke.com/`)//On intercepte les éventuelles erreurs et on affiche un message
-    // dans la boîte `blague`.
-})
+//USING ASYNC/AWAIT
+async function generateJoke(){
+        const config = {
+        headers:{
+            'Accept': 'application/json'
+        }
+        }
+
+        const res = await fetch('https://icanhazdadjoke.com',config)
+        
+        const data = await res.json()
+        
+        jokeEl.innerHTML = data.joke
+    }
 
 
+  //USING .then()  
 
+// function generateJoke(){
+//        const config = {
+//         headers:{
+//             'Accept': 'application/json'
+//         }
+//        }
+
+//        fetch('https://icanhazdadjoke.com',config)
+//        .then((res) => res.json())
+//        .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//        })
+// }
 
